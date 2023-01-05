e6c10462c0d1ba2cac723b461b2a70a609b23868
mshekerotokiti <sharontokiti@gmail.com>
2023-01-05 09:55:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//math operators
+const year = 2040;
+const ageSharon = year - 1999; //41
+const ageMilan = year - 2020;  //20
+console.log (ageSharon, ageMilan);
+
+console.log (ageSharon / 2, ageMilan *2/10);
+//ageMilan*2/10 means multiply by 2 then divide by 10
+
+let sex = "female";
+console.log (typeof sex);
+
+let PI = 3.1415;
+console.log (typeof PI);
+
+const firstName = "biryani ya";
+const lastName = "kuku";
+console.log (firstName +''+ lastName);
+
+//assignment operators
+let x = 10+5; //15
+x= x+=10; //25
+x++ // adds 1 to the figure
+x--//subtracts 1 from the answer
+x=x/5 //divides answer by 5 which gives 5
+console. log (x);
+
+//comparison operators should give boolean expression
+console.log (ageSharon > ageMilan); //true
+console.log (year-1999 < year-2020); //false
+console.log (ageMilan>=22); //false
+
+
+
